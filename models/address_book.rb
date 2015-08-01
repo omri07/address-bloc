76aa532ac113774ed12a08565ca3d35d5036c46f
Omri Hamo <omripri@gmail.com>
2015-08-01 15:37:29 +0300

    checkpint-4

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,6 +1,7 @@
  # #8
 require_relative "entry.rb"
- 
+require "csv"
+
 class AddressBook
     attr_accessor :entries
 
@@ -25,4 +26,15 @@ class AddressBook
     def remove_entry(email)
       @entries.delete_if {|entry| entry.email == email}
     end
+
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+ # #8
+      csv.each do |row|
+        row_hash = row.to_hash
+        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      end
+    end
 end
+
